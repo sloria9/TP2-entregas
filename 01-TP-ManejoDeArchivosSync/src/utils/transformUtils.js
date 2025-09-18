89a1a1992b7c8462fe8c06a785ab834cb791cf4a
sofia <sofia.loria9@gmail.com>
2025-09-18 18:37:42 -0300

    escribirTextoEnArchivo

--- 01-TP-ManejoDeArchivosSync/src/utils/transformUtils.js
+++ 01-TP-ManejoDeArchivosSync/src/utils/transformUtils.js
@@ -1,6 +1,11 @@
 /**
  * separa el string ante cada ocurrencia del separador, y agrega al array resultado siempre que pueda
  * transformar al fragmento de string en numero.
+ * 
+ * 
+ * Recibe un texto y una secuencia de caracteres que usará como separador.
+ * Devuelve un array con todos los números producto de partir el texto cada vez que aparezca la secuencia separadora.
+ * En el caso de que alguna de las partes no sea numérica, no se incluirá en el resultado, pero no debe lanzar ningún error.
  * @param {string} str 
  * @param {string} separador
  * @returns {number[]} array de numeros
