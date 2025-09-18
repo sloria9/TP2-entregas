1c300e9fa0af162762bf74dceccaa2c64fbafe4a
sofia <sofia.loria9@gmail.com>
2025-09-18 18:37:31 -0300

    escribirTextoEnArchivo

--- 01-TP-ManejoDeArchivosSync/src/utils/fileUtils.js
+++ 01-TP-ManejoDeArchivosSync/src/utils/fileUtils.js
@@ -11,11 +11,35 @@ export function leerArchivoComoString(ruta) {
 }
 
 /**
- * escribe el texto en el archivo de la ruta, sólo si tal archivo existe. sino, lanza error.
+ * escribe el texto en el archivo de la ruta, sólo si tal archivo existe.
+ * sino, lanza error.
+ * flag en true, crea el archivo y lo escribe.
+ * flag en false, lanza el error “el archivo no existe”.
  * @param {string} ruta relativa al directorio del proyecto
  * @param {string} texto 
  */
 export function escribirTextoEnArchivo(ruta, texto, shouldCreateIfNotExists) {
+
+  try {
+    if (fs.existsSync(ruta)) { //valida si existe un archivo en esa ruta exacta
+      // El archivo ya existe escribo
+      console.log('Contenido actual:', fs.readFileSync(ruta, 'utf-8'))
+      console.log('Escribiendo archivo..')
+      fs.writeFileSync(ruta, texto, "utf-8");
+
+      console.log(`Archivo sobrescrito en: ${ruta}`);
+      console.log('Contenido actualizado:', fs.readFileSync(ruta, 'utf-8'))
+    } else if(shouldCreateIfNotExists) {
+      //si el archivo no existe
+        console.log(`Debi crear archivo en la ruta: ${ruta}`);
+        fs.writeFileSync(ruta, texto, "utf-8"); // lo crea automáticamente
+        console.log(`Archivo creado en: ${ruta}`);
+    } else {
+        throw new Error("El archivo no existe.")
+    }
+  } catch (error) {
+    console.error("Error al escribir el archivo:", error.message);
+  }
 }
 
 // exportar ambas funciones
