4203e56b5bcf340ed0e35c4fb2a64aa26bcb44d7
sofia <sofia.loria9@gmail.com>
2025-09-18 18:43:59 -0300

    transformUtils completed

--- 01-TP-ManejoDeArchivosSync/src/utils/transformUtils.js
+++ 01-TP-ManejoDeArchivosSync/src/utils/transformUtils.js
@@ -11,6 +11,10 @@
  * @returns {number[]} array de numeros
  */
 export function transformarStringEnArrayDeNumeros(str, separador) {
+      return str
+    .split(separador)              // divide el string en el separador
+    .map(parte => Number(parte))   // 'castea' cada parte en número (NaN si no se puede)
+    .filter(num => !isNaN(num))   // descarta los NaN (func que detecta si son numero)
 }
 
 /**
@@ -20,5 +24,7 @@ export function transformarStringEnArrayDeNumeros(str, separador) {
  * @returns {string} el nuevo string
  */
 export function transformarArrayDeNumerosAUnSoloString(arr, separador) {
+    return arr
+    .join(separador) // join does the reverse to split, it creates a string of arr items joined by separator between them.
 }
 
