28b6355b49b3d5d718370c4981305946f6f4b6f7
sofia <sofia.loria9@gmail.com>
2025-09-18 19:35:14 -0300

    apareo class

--- 01-TP-ManejoDeArchivosSync/src/apareo.js
+++ 01-TP-ManejoDeArchivosSync/src/apareo.js
@@ -6,6 +6,40 @@
  * @returns {number[]} un nuevo array de números ordenados
  */
 export function combinarDosArrays(arrA, arrB) {
+
+    let indexA = 0
+    let indexB = 0
+    const apareada = [] //guarda resultado final
+
+  while (indexA < arrA.length && indexB < arrB.length) { //itero mientras haya contenido
+    const a = arrA[indexA];
+    const b = arrB[indexB];
+
+    if (a < b) {
+      apareada.push(a)
+      indexA++;
+    } else if (a > b) {
+      apareada.push(b)
+      indexB++;
+    } else { //si son iguales pusheo cualquiera pero me quedo con uno
+        apareada.push(a)
+        indexA++;
+        indexB++;
+    }
+  }
+
+  // agregar todo lo que quede de alguno de los arrays directamente
+  while (indexA < arrA.length) {
+    apareada.push(arrA[indexA]);
+    indexA++;
+  }
+  while (indexB < arrB.length) {
+    apareada.push(arrB[indexB]);
+    indexB++;
+  }
+
+  return apareada;
+
 }
 
 /**
@@ -14,6 +48,13 @@ export function combinarDosArrays(arrA, arrB) {
  * @returns {nuber[]} el nuevo array de números ordenados
  */
 export function combinarNArrays(arrs) {
+      if (arrs.length === 0) return [];
+  if (arrs.length === 1) return arrs[0];
+
+  // reduce arranca con el primer array como acumulador
+  return arrs.reduce((acc, current) => { //We use reduce when we need to iterate over an array
+    return combinarDosArrays(acc, current);
+  });
 }
 
 // exportar ambas funciones
